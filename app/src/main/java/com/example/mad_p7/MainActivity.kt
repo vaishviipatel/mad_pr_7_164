5727005824036f715ab609429ede7b5ac661bb61
gunjan patel <g.a.patel359@gmail.com>
2024-11-18 21:45:10 +0530

    completed

--- app/src/main/java/com/example/mad_p7/MainActivity.kt
+++ app/src/main/java/com/example/mad_p7/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mad_p7
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,6 +31,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btn=findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
 
         // Initialize RecyclerView
--- app/src/main/java/com/example/mad_p7/Person.kt
+++ app/src/main/java/com/example/mad_p7/Person.kt
@@ -9,26 +9,20 @@ data class Person(
     val emailId: String,
     val phoneNo: String,
     val address: String,
-    val latitude: Double,
-    val longitude: Double
+    val latitude: Double?,
+    val longitude: Double?
 ) : Serializable {
     companion object {
         fun fromJson(jsonObject: JSONObject): Person {
-            // Extracting main fields
             val id = jsonObject.getString("id")
             val emailId = jsonObject.getString("email")
             val phoneNo = jsonObject.getString("phone")
-
-            // Extracting nested fields
             val profileObject = jsonObject.getJSONObject("profile")
             val name = profileObject.getString("name")
             val address = profileObject.getString("address")
-
             val locationObject = profileObject.getJSONObject("location")
             val latitude = locationObject.getDouble("lat")
             val longitude = locationObject.getDouble("long")
-
-            // Creating and returning the Person object
             return Person(
                 id = id,
                 name = name,
